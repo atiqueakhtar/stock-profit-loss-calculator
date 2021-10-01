6b787a2945c530e3a1fbfb80db003e3044f7d801
Atique Akhtar <48538467+atiqueakhtar@users.noreply.github.com>
2021-10-01 12:59:25 +0530

    fix: profit/loss percent formula

--- src/components/Button.js
+++ src/components/Button.js
@@ -6,14 +6,14 @@ export default function (props) {
 
     let profit = currentPrice * quantity - initialPrice * quantity;
     if (profit > 0) {
-      let profitPercent = (profit / (currentPrice * quantity)) * 100;
+      let profitPercent = (profit / (initialPrice * quantity)) * 100;
       document.querySelector("#result").classList.remove("result-loss");
       document.querySelector("#result").classList.add("result-profit");
       props.setResult(
         `Yayy! The profit is ${profit} and percent is ${profitPercent}%`
       );
     } else if (profit < 0) {
-      let lossPercent = -(profit / (currentPrice * quantity)) * 100;
+      let lossPercent = -(profit / (initialPrice * quantity)) * 100;
       document.querySelector("#result").classList.remove("result-profit");
       document.querySelector("#result").classList.add("result-loss");
       props.setResult(
